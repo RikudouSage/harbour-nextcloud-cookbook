dbb01e5e23f039e24f60e56b2922d33e5d4c32fb
Frank Paul Silye <frankps@gmail.com>
2026-08-03 13:30:04 +0200

    Norwegian bokmål translations
    Translations for the app completed.

---
+++ translations/harbour-nextcloud-cookbook-no_NB.ts
@@ -0,0 +1,289 @@
+‭+47 413 81 029<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name></name>
+    <message id="global.loading">
+        <source>Loading...</source>
+        <translation>Laster inn...</translation>
+    </message>
+    <message id="check_page.title">
+        <source>Checking...</source>
+        <translation>Sjekker...</translation>
+    </message>
+    <message id="login.login">
+        <source>Log In</source>
+        <translation>Logg inn</translation>
+    </message>
+    <message id="dialog.cancel">
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message id="login.username">
+        <source>Username</source>
+        <translation>Brukernavn</translation>
+    </message>
+    <message id="login.password">
+        <source>Password</source>
+        <translation>Passord</translation>
+    </message>
+    <message id="login.nextcloud_url">
+        <source>Nextcloud URL</source>
+        <translation>Nextcloud-URL</translation>
+    </message>
+    <message id="check.validating_credentials">
+        <source>Validating credentials...</source>
+        <translation>Validerer innloggingsopplysninger...</translation>
+    </message>
+    <message id="check.invalid_credentials">
+        <source>Invalid username or password</source>
+        <translation>Ugyldig brukernavn eller passord</translation>
+    </message>
+    <message id="check.initializing">
+        <source>Initializing...</source>
+        <translation>Initialiserer...</translation>
+    </message>
+    <message id="check.failed_initializing">
+        <source>There was an internal error while initializing. There&apos;s nothing you can do except reporting it to the developer.</source>
+        <translation>Det oppstod en intern feil under initialisering ved innlogging. Rapporter dette til utvikleren.</translation>
+    </message>
+    <message id="main.title">
+        <source>Recipes</source>
+        <translation>Oppskrifter</translation>
+    </message>
+    <message id="main.loading_recipes">
+        <source>Loading recipes...</source>
+        <translation>Laster inn oppskrifter...</translation>
+    </message>
+    <message id="main.recipes_error">
+        <source>Could not load recipes. Pull down to refresh.</source>
+        <translation>Kunne ikke laste inn oppskrifter. Dra ned for å oppdatere.</translation>
+    </message>
+    <message id="main.import_error">
+        <source>Could not import recipe.</source>
+        <translation>Kunne ikke importere oppskrift.</translation>
+    </message>
+    <message id="main.delete_error">
+        <source>Could not delete recipe.</source>
+        <translation>Kunne ikke slette oppskrift.</translation>
+    </message>
+    <message id="main.import_from_url">
+        <source>Import from URL</source>
+        <translation>Importer fra URL</translation>
+    </message>
+    <message id="main.refresh">
+        <source>Refresh</source>
+        <translation>Oppdater</translation>
+    </message>
+    <message id="main.logout">
+        <source>Logout</source>
+        <translation>Logg ut</translation>
+    </message>
+    <message id="main.search_recipes">
+        <source>Search recipes</source>
+        <translation>Søk i oppskrifter</translation>
+    </message>
+    <message id="main.empty">
+        <source>No recipes</source>
+        <translation>Ingen oppskrifter</translation>
+    </message>
+    <message id="recipe_item.delete">
+        <source>Delete</source>
+        <translation>Slett</translation>
+    </message>
+    <message id="categories.title">
+        <source>Categories</source>
+        <translation>Kategorier</translation>
+    </message>
+    <message id="categories.loading">
+        <source>Loading categories...</source>
+        <translation>Laster inn kategorier...</translation>
+    </message>
+    <message id="categories.error">
+        <source>Could not load categories. Pull down to refresh.</source>
+        <translation>Kunne ikke laste inn kategorier. Dra ned for å oppdatere.</translation>
+    </message>
+    <message id="categories.empty">
+        <source>No categories</source>
+        <translation>Ingen kategorier</translation>
+    </message>
+    <message id="categories.uncategorized">
+        <source>Uncategorized</source>
+        <translation>Ukategorisert</translation>
+    </message>
+    <message id="main.search_error">
+        <source>Could not search recipes.</source>
+        <translation>Kunne ikke søke etter oppskrifter.</translation>
+    </message>
+    <message id="tags.title">
+        <source>Tags</source>
+        <translation>Emneknagger</translation>
+    </message>
+    <message id="tags.loading">
+        <source>Loading tags...</source>
+        <translation>Laster inn emneknagger...</translation>
+    </message>
+    <message id="tags.error">
+        <source>Could not load tags. Pull down to refresh.</source>
+        <translation>Kunne ikke laste inn emneknagger. Dra ned for å oppdatere.</translation>
+    </message>
+    <message id="tags.empty">
+        <source>No tags</source>
+        <translation>Ingen emneknagger</translation>
+    </message>
+    <message id="import_recipe.importing">
+        <source>Importing recipe...</source>
+        <translation>Importerer oppskrift...</translation>
+    </message>
+    <message id="import_recipe.url">
+        <source>Recipe URL</source>
+        <translation>Oppskrifts-URL</translation>
+    </message>
+    <message id="category_detail.renaming">
+        <source>Renaming category...</source>
+        <translation>Endrer navn på kategori...</translation>
+    </message>
+    <message id="category_detail.rename_error">
+        <source>Could not rename category.</source>
+        <translation>Kunne ikke endre navn på kategori.</translation>
+    </message>
+    <message id="category_detail.rename">
+        <source>Rename category</source>
+        <translation>Endre navn på kategori</translation>
+    </message>
+    <message id="category_detail.category_name">
+        <source>Category name</source>
+        <translation>Kategorinavn</translation>
+    </message>
+    <message id="recipe_detail.loading">
+        <source>Loading recipe...</source>
+        <translation>Laster inn oppskrift...</translation>
+    </message>
+    <message id="recipe_detail.nutrition.calories">
+        <source>Calories</source>
+        <translation>Kalorier</translation>
+    </message>
+    <message id="recipe_detail.nutrition.carbohydrate">
+        <source>Carbohydrate</source>
+        <translation>Karbohydrater</translation>
+    </message>
+    <message id="recipe_detail.nutrition.cholesterol">
+        <source>Cholesterol</source>
+        <translation>Kolesterol</translation>
+    </message>
+    <message id="recipe_detail.nutrition.fat">
+        <source>Fat</source>
+        <translation>Fett</translation>
+    </message>
+    <message id="recipe_detail.nutrition.fiber">
+        <source>Fiber</source>
+        <translation>Kostfiber</translation>
+    </message>
+    <message id="recipe_detail.nutrition.protein">
+        <source>Protein</source>
+        <translation>Protein</translation>
+    </message>
+    <message id="recipe_detail.nutrition.saturated_fat">
+        <source>Saturated fat</source>
+        <translation>Mettet fett</translation>
+    </message>
+    <message id="recipe_detail.nutrition.serving_size">
+        <source>Serving size</source>
+        <translation>Porsjonsstørrelse</translation>
+    </message>
+    <message id="recipe_detail.nutrition.sodium">
+        <source>Sodium</source>
+        <translation>Natrium</translation>
+    </message>
+    <message id="recipe_detail.nutrition.sugar">
+        <source>Sugar</source>
+        <translation>Sukker</translation>
+    </message>
+    <message id="recipe_detail.nutrition.trans_fat">
+        <source>Trans fat</source>
+        <translation>Transfett</translation>
+    </message>
+    <message id="recipe_detail.nutrition.unsaturated_fat">
+        <source>Unsaturated fat</source>
+        <translation>Umettet fett</translation>
+    </message>
+    <message id="recipe_detail.preparation_time">
+        <source>Preparation</source>
+        <oldsource>Preparation time</oldsource>
+        <translation>Forberedelse</translation>
+    </message>
+    <message id="recipe_detail.cooking_time">
+        <source>Cooking</source>
+        <oldsource>Cooking time</oldsource>
+        <translation>Tilberedningstid</translation>
+    </message>
+    <message id="recipe_detail.total_time">
+        <source>Total time</source>
+        <translation>Total tid</translation>
+    </message>
+    <message id="recipe_detail.error">
+        <source>Could not load recipe. Pull down to refresh.</source>
+        <translation>Kunne ikke laste inn oppskrift. Dra ned for å oppdatere.</translation>
+    </message>
+    <message id="recipe_detail.source">
+        <source>Source: %1</source>
+        <translation>Kilde: %1</translation>
+    </message>
+    <message id="recipe_detail.servings">
+        <source>Servings: %1</source>
+        <translation>Porsjoner: %1</translation>
+    </message>
+    <message id="recipe_detail.ingredients">
+        <source>Ingredients</source>
+        <translation>Ingredienser</translation>
+    </message>
+    <message id="recipe_detail.instructions">
+        <source>Instructions</source>
+        <translation>Fremgangsmåte</translation>
+    </message>
+    <message id="recipe_detail.tools">
+        <source>Tools</source>
+        <translation>Redskaper</translation>
+    </message>
+    <message id="recipe_detail.nutrition">
+        <source>Nutrition Information</source>
+        <translation>Næringsinnhold</translation>
+    </message>
+    <message id="recipe_detail.empty">
+        <source>No recipe details</source>
+        <translation>Ingen detaljer for oppskrift</translation>
+    </message>
+    <message id="recipe_detail.created">
+        <source>Created</source>
+        <translation>Opprettet</translation>
+    </message>
+    <message id="recipe_detail.modified">
+        <source>Modified</source>
+        <translation>Endret</translation>
+    </message>
+    <message id="recipe_detail.category">
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message id="recipe_detail.durations">
+        <source>Durations</source>
+        <translation>Tidsbruk</translation>
+    </message>
+    <message id="recipe_detail.step">
+        <source>Step %1</source>
+        <translation>Steg %1</translation>
+    </message>
+    <message id="recipe_detail.keep_screen_on">
+        <source>Keep screen on</source>
+        <translation>Hold skjermen på</translation>
+    </message>
+    <message id="global.request_debug.disable">
+        <source>Disable debugging</source>
+        <translation>Deaktiver feilsøking</translation>
+    </message>
+    <message id="global.request_debug.enable">
+        <source>Enable debugging</source>
+        <translation>Aktiver feilsøking</translation>
+    </message>
+</context>
+</TS>
